792789c7c0acb5ae01c1ab65092dccdb8eda78a5
Manu(Manu11-Pro) <194583214+Manu11-Pro@users.noreply.github.com>
2026-03-21 22:09:47 +0530

    Add files via upload

---
+++ Bohr_Model_of_Atom (1).py
@@ -0,0 +1,43 @@
+import time
+import viser
+import trimesh
+
+def main():
+    server = viser.ViserServer(host="0.0.0.0", port=7860)
+    server.scene.add_icosphere(
+        name="/nucleus",
+        radius=1.0,
+        color=(255,0,0),
+        position=(0.0,0.0,0.0)
+    )
+    
+    server.scene.add_icosphere(
+        name="/electron1",
+        radius=0.5,
+        color=(0,0,255),
+        position=(2.0,0.0,0.0)
+    )
+
+    server.scene.add_icosphere(
+        name="/electron2",
+        radius=0.5,
+        color=(0,0,255),
+        position=(-2.0,0.0,0.0)
+    )
+
+    electron_ring_mesh=trimesh.creation.annulus(r_min=2.0, r_max=2.1, height=0.1)
+    server.scene.add_mesh_trimesh(
+        name="/electron_ring",
+        mesh=electron_ring_mesh
+    )
+
+    server.gui.add_markdown("***Bohr_Model_of_Atom***")
+
+    print("Server running on Hugging Face port 7860")
+    
+    while True:
+        time.sleep(10.0)
+
+
+if __name__ == "__main__":
+    main()
